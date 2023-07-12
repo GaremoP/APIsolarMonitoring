4e312de8007a7cdb89c3e919c168e150d4a3aaf5
Pablo <uo270546@uniovi.es>
2023-07-12 08:48:46 +0000

    Final changes to routes. Swagger production mode

--- FilesPaths.cs
+++ FilesPaths.cs
@@ -2,10 +2,10 @@
 {
     public static class FilesPaths
     {
-        public const string Datalogger1 = "Datalogger1.csv";
-        public const string Datalogger2 = "Datalogger2.csv";
-        public const string Dataloger2hLocal24 = "Datalogger24hLocal.csv";
-        public const string Datalogger1hLocal24 = "Datalogger24hLocalEstaci.csv";
-        public const string InverterString1 = "InverterData.csv";
+        public const string Datalogger1 = "C:\\Users\\ServidorCEDS\\Documents\\MonitorizacionPV\\TratarDatos\\DatosTratados\\Datalogger1.csv";
+        public const string Datalogger2 = "C:\\Users\\ServidorCEDS\\Documents\\MonitorizacionPV\\TratarDatos\\DatosTratados\\Datalogger2.csv";
+        public const string Dataloger2hLocal24 = "C:\\Users\\ServidorCEDS\\Documents\\MonitorizacionPV\\TratarDatos\\DatosTratados\\Datalogger24hLocal.csv";
+        public const string Datalogger1hLocal24 = "C:\\Users\\ServidorCEDS\\Documents\\MonitorizacionPV\\TratarDatos\\DatosTratados\\Datalogger24hLocalEstaci.csv";
+        public const string InverterString1 = "C:\\Users\\ServidorCEDS\\Documents\\MonitorizacionPV\\TratarDatos\\DatosTratados\\InverterDataT1.csv";
     }
 }
